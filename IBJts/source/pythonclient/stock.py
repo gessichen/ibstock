5807a44d0e24da95b66b819b21c729edf1be8bc3
cheguo <cheguo@microsoft.com>
2020-05-27 13:16:09 -0700

    check stocks

--- IBJts/source/pythonclient/stock.py
+++ IBJts/source/pythonclient/stock.py
@@ -3,18 +3,65 @@ from IBJts.source.pythonclient.ibapi.wrapper import EWrapper
 from IBJts.source.pythonclient.ibapi.contract import Contract
 from IBJts.source.pythonclient.ibapi.order import Order
 
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+import datetime
+import json
+
 class StockApp(EWrapper, EClient):
 
     def __init__(self):
         EClient.__init__(self, self)
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(checkStocks, 'interval', seconds=3)
+        scheduler.start()
 
     def error(self, reqId, errorCode, errorString):
         print("Error: ", reqId, errorCode, " ", errorString)
 
-    def orderStatus(self, orderId: OrderId, status: str, filled: float,
-                    remaining: float, avgFillPrice: float, permId: int,
-                    parentId: int, lastFillPrice: float, clientId: int,
-                    whyHeld: str, mktCapPrice: float):
+    def checkStocks(self):
+        now = datetime.datetime.now()
+        suburl = "immediate"
+
+        if now > datetime.datetime(now.year, now.month, now.day, 14, 34, 50):
+            suburl = "scheduled"
+
+        headers = {
+            "content-type": "application/json",
+        }
+        url = "http://localhost:5012/trade/" + suburl + "?trader=ib"
+        params = {}
+
+        try:
+            response = requests.get(url, params=params, headers=headers)
+            stock = response.json().get("stock")
+
+            if stock != "":
+
+                self.buyStock(stock)
+                orderData = {
+                    "trade": response.json().get("trade"),
+                    "stock": response.json().get("stock"),
+                    "status": "success",
+                    "company": response.json().get("company"),
+                    "amount": 500
+                }
+                orderUrl = "http://localhost:5012/order?trader=ib"
+                resp1 = requests.post(orderUrl, params=params, data=json.dumps(orderData), headers=headers)
+
+                orderCountData = {
+                    "trade": response.json().get("trade"),
+                }
+                orderCountUrl = "http://localhost:5012/order/count?trader=ib"
+                resp2 = requests.post(orderCountUrl, params=params, data=json.dumps(orderCountData), headers=headers)
+
+        except requests.ConnectionError:
+            print("cannot request")
+
+    def orderStatus(self, orderId, status, filled,
+                    remaining, avgFillPrice, permId,
+                    parentId, lastFillPrice, clientId,
+                    whyHeld, mktCapPrice):
         super().orderStatus(orderId, status, filled, remaining,
                             avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice)
         print("OrderStatus. Id:", orderId, "Status:", status, "Filled:", filled,
@@ -39,7 +86,8 @@ class StockApp(EWrapper, EClient):
 
 
 def main():
-    app = StockApp()
-    app.connect("127.0.0.1", 7497, 1)
+    #app = StockApp()
+    #app.connect("127.0.0.1", 7497, 1)
 
-    app.run()
+    #app.run()
+    print("run")
